52fa01ffb6c63cead3032b92fdf58c51953c34b1
Daniel Levi <daniellevi000@gmail.com>
2024-05-23 21:47:44 -0300

    feat: create route get profile

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,7 +1,7 @@
 import { Elysia, t, type Static } from 'elysia'
 import jwt from '@elysiajs/jwt'
-
 import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
 const jwtPayload = t.Object({
   id: t.String(),
@@ -15,19 +15,30 @@ export const auth = new Elysia()
       schema: jwtPayload,
     }),
   )
-  .derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
-    return {
-      signUser: async (payload: Static<typeof jwtPayload>) => {
-        const token = await jwt.sign(payload)
+  .use(cookie())
+  .derive({ as: 'scoped' }, ({ jwt, cookie }) => ({
+    signUser: async (payload: Static<typeof jwtPayload>) => {
+      const token = await jwt.sign(payload)
 
-        auth.value = token
-        auth.httpOnly = true
-        auth.maxAge = 60 * 60 * 24 * 7 // 7 days
-        auth.path = '/'
-      },
+      cookie.auth.value = token
+      cookie.auth.httpOnly = true
+      cookie.auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+      cookie.auth.path = '/'
+    },
 
-      signOut: async () => {
-        auth.remove()
-      },
-    }
-  })
+    signOut: async () => {
+      cookie.auth.remove()
+    },
+
+    getCurrentUser: async () => {
+      const payload = await jwt.verify(cookie.auth.value)
+      if (!payload) {
+        throw new Error('Unauthorized.')
+      }
+
+      return {
+        userId: payload.id,
+        restaurantId: payload.restaurantId,
+      }
+    },
+  }))
--- src/http/server.ts
+++ src/http/server.ts
@@ -3,12 +3,15 @@ import { registerRestaurant } from './route/register-restaurant'
 import { sendAuthLink } from './route/send-auth-link'
 import { authenticateFromLink } from './route/authenticate-from-link'
 import { signOut } from './route/sign-out'
+import { getProfile } from './route/get-profile'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
+
 app.listen(3333, () => {
   console.log('Server is running on port 3333')
 })
