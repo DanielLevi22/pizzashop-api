f9add1d7d92ddd8e4b225370cd3bc50c6ea114ce
Daniel Levi <daniellevi000@gmail.com>
2024-05-24 22:53:04 -0300

    feat: create route get daily receipt in periode

--- src/http/server.ts
+++ src/http/server.ts
@@ -16,6 +16,7 @@ import { getDayOrdersAmount } from './route/get-days-orders-amount'
 import { getMonthOrdersAmount } from './route/get-month-orders-amount'
 import { getMonthCanceledOrdersAmount } from './route/get-canceled-month-orders-amount'
 import { getPopularProducts } from './route/get-popular-products'
+import { getDailyReceiptInPeriod } from './route/get-daily-receipt-in-period'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -35,6 +36,7 @@ const app = new Elysia()
   .use(getMonthOrdersAmount)
   .use(getMonthCanceledOrdersAmount)
   .use(getPopularProducts)
+  .use(getDailyReceiptInPeriod)
 
   .onError(({ code, set, error }) => {
     switch (code) {
