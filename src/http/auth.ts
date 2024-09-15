3a76af8d928b28f769fcd1d91e853c37cbde7a48
Daniel Levi <daniellevi000@gmail.com>
2024-09-15 19:00:54 -0300

    fixzx: bug

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -50,6 +50,7 @@ export const auth = new Elysia()
       cookie.auth.httpOnly = true
       cookie.auth.maxAge = 60 * 60 * 24 * 7 // 7 days
       cookie.auth.path = '/'
+      cookie.auth.secure = process.env.NODE_ENV === 'production'
     },
 
     signOut: async () => {
